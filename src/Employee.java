004d0696813e1b291c5ba6eebbce2dc2be774d75
aslisimsek <simseekasli@gmail.com>
2022-06-25 16:09:22 +0300

    -Calculate student note and avarage -Show student pass or fail

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    String name;
+    int sallary;
+    int workHours;
+    int hireYear;
+
+    Employee(String name, int sallary, int workHours, int hireYear) {
+        this.name = name;
+        this.sallary = sallary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public int calculatedTax() {
+        if (sallary < 1000) {
+            return 0;
+        } else {
+            return (int) (sallary * 0.03);
+        }
+    }
+
+    public int calculatedBonus() {
+        if (workHours > 40) {
+            return (workHours - 40) * 30;
+        }
+        return 0;
+    }
+
+    public int raiseSallary() {
+        int totalWorkYear = 2022 - hireYear;
+        if (totalWorkYear < 10) {
+            return (int) (sallary * 0.05);
+        } else if (totalWorkYear > 10 && totalWorkYear < 20) {
+            return (int) (sallary * 0.10);
+        } else {
+            return (int) (sallary * 0.15);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name='" + name + '\'' +
+                ", sallary=" + sallary +
+                ", workHours=" + workHours +
+                ", hireYear=" + hireYear +
+                '}';
+    }
+}
---
+++ src/SallaryCalculator.java
@@ -0,0 +1,4 @@
+public class SallaryCalculator {
+    public static void main(String[] args) {
+    }
+}
